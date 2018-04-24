f37cf8a91c8bd0b18991d918623a83352355c8b3
EgorLobach <mega.lobach@gmail.com>
2018-04-24 12:30:30 +0300

    delete comments

--- ui_components/src/components/alloy/static/components/js/chemistry_components/alloy_ui_component.js
+++ ui_components/src/components/alloy/static/components/js/chemistry_components/alloy_ui_component.js
@@ -34,7 +34,6 @@ AlloyUIComponent.PaintPanel.prototype = {
         container.append('<button id="newButton" type="button">Найти компоненты заданного сплава</button>');
         container.append('<button id="searchInfoButton" type="button">Посмотреть раздел Сплавы</button>');
         container.append('<button id="generateNodes" type="button">Посмотреть упражнения на Сплавы</button>');
-        //If you don't want to make default command - add class="sc-no-default-cmd" to button
 
         $('#newButton').click(function () {
 			self._findAlloyComponent();
@@ -49,28 +48,18 @@ AlloyUIComponent.PaintPanel.prototype = {
 			self._findAlloyExercises();
 		});
     },
-
-    /* Call agent of searching semantic neighborhood,
-	send ui_main_menu node as parameter and add it in web window history
-	*/
 	_findAlloyComponent: function () {
 		var addr;
-		// Resolve sc-addr. Get sc-addr of ui_main_menu node
 		SCWeb.core.Server.resolveScAddr([SCWeb.core.Arguments._arguments[0]], function (keynodes) {
 			console.log("key", keynodes);
 			console.log("arguments", SCWeb.core.Arguments._arguments[0]);
 			addr = SCWeb.core.Arguments._arguments[0];
-			// Resolve sc-addr of ui_menu_view_full_semantic_neighborhood node
 			SCWeb.core.Server.resolveScAddr(["ui_menu_find_alloy_components"],
 			function (data) {
-				// Get command of ui_menu_view_full_semantic_neighborhood
 				var cmd = data["ui_menu_find_alloy_components"];
-				// Simulate click on ui_menu_view_full_semantic_neighborhood button
 				SCWeb.core.Main.doCommand(cmd,
 				[addr], function (result) {
-					// waiting for result
 					if (result.question != undefined) {
-						// append in history
 						SCWeb.ui.WindowManager.appendHistoryItem(result.question);
 					}
 				});
@@ -80,20 +69,14 @@ AlloyUIComponent.PaintPanel.prototype = {
 
 	_findSectionOfAlloys: function () {
 		var addr;
-		// Resolve sc-addr. Get sc-addr of ui_main_menu node
 		SCWeb.core.Server.resolveScAddr(['section_subject_domain_of_alloys'], function (keynodes) {
 			addr = keynodes['section_subject_domain_of_alloys'];
-			// Resolve sc-addr of ui_menu_view_full_semantic_neighborhood node
 			SCWeb.core.Server.resolveScAddr(["ui_menu_view_full_semantic_neighborhood"],
 			function (data) {
-				// Get command of ui_menu_view_full_semantic_neighborhood
 				var cmd = data["ui_menu_view_full_semantic_neighborhood"];
-				// Simulate click on ui_menu_view_full_semantic_neighborhood button
 				SCWeb.core.Main.doCommand(cmd,
 				[addr], function (result) {
-					// waiting for result
 					if (result.question != undefined) {
-						// append in history
 						SCWeb.ui.WindowManager.appendHistoryItem(result.question);
 					}
 				});
@@ -103,20 +86,14 @@ AlloyUIComponent.PaintPanel.prototype = {
 
 	_findAlloyExercises: function () {
 		var addr;
-		// Resolve sc-addr. Get sc-addr of ui_main_menu node
 		SCWeb.core.Server.resolveScAddr(['exercises_mixture'], function (keynodes) {
 			addr = keynodes['exercises_mixture'];
-			// Resolve sc-addr of ui_menu_view_full_semantic_neighborhood node
 			SCWeb.core.Server.resolveScAddr(["ui_menu_view_full_semantic_neighborhood"],
 			function (data) {
-				// Get command of ui_menu_view_full_semantic_neighborhood
 				var cmd = data["ui_menu_view_full_semantic_neighborhood"];
-				// Simulate click on ui_menu_view_full_semantic_neighborhood button
 				SCWeb.core.Main.doCommand(cmd,
 				[addr], function (result) {
-					// waiting for result
 					if (result.question != undefined) {
-						// append in history
 						SCWeb.ui.WindowManager.appendHistoryItem(result.question);
 					}
 				});
@@ -151,15 +128,12 @@ AlloyUIComponent.DrawWindow = function (sandbox) {
 
     function drawAllElements() {
         var dfd = new jQuery.Deferred();
-       // for (var addr in scElements) {
             jQuery.each(scElements, function(j, val){
                 var obj = scElements[j];
                 if (!obj || obj.translated) return;
-// check if object is an arc
                 if (obj.data.type & sc_type_arc_pos_const_perm) {
                     var begin = obj.data.begin;
                     var end = obj.data.end;
-                    // logic for component update should go here
                 }
 
         });
@@ -168,19 +142,15 @@ AlloyUIComponent.DrawWindow = function (sandbox) {
         return dfd.promise();
     }
 
-// resolve keynodes
     var self = this;
     this.needUpdate = false;
     this.requestUpdate = function () {
         var updateVisual = function () {
-// check if object is an arc
             var dfd1 = drawAllElements();
             dfd1.done(function (r) {
                 return;
             });
 
-
-/// @todo: Don't update if there are no new elements
             window.clearTimeout(self.structTimeout);
             delete self.structTimeout;
             if (self.needUpdate)
@@ -215,7 +185,6 @@ AlloyUIComponent.DrawWindow = function (sandbox) {
             });
         });
     };
-// delegate event handlers
     this.sandbox.eventDataAppend = $.proxy(this.receiveData, this);
     this.sandbox.eventStructUpdate = $.proxy(this.eventStructUpdate, this);
     this.sandbox.updateContent();
